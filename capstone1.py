7e8feb5ca959ef9d21fb3ceaa5e1d4e2c6b0a0d9
Rikza <fachry240702@gmail.com>
2025-02-15 19:43:52 +0700

    Fix

--- capstone1.py
+++ capstone1.py
@@ -2,8 +2,7 @@
 
 from tabulate import tabulate
 
-# 1. MEMBUAT DAFTAR MOBIL
-
+# 1. MEMBUAT DAFTAR MOBIL 
 mobil_list = [
     {"ID": 'E1', "Jenis": "Hiace", "Transmisi": "MT", "Harga": 500000, "Kapasitas": 11, "Stok": 5},
     {"ID": 'J2', "Jenis": "Jazz", "Transmisi": "AT", "Harga": 400000, "Kapasitas": 4, "Stok": 3},
@@ -21,11 +20,12 @@ def transmisi():
         transmisi_baru = validasi_alfabet('Masukkan Transmisi (AT/MT): ').upper().strip()
         if transmisi_baru in ['AT', 'MT']:
             return transmisi_baru  # Keluar dari loop jika input valid
-        print('Masukkan data yang valid! (AT atau MT)') 
+        print('\nMasukkan data yang valid! (AT atau MT)') 
 
 # FUNGSI RANDOM ANGKA
 import random
 def generate_angka(jenis):
+
     if mobil_list:  # Cek apakah mobil_list tidak kosong
         angka_terakhir = max(int(mobil["ID"][1:]) for mobil in mobil_list)  # Ambil angka terbesar dari ID
     else:
@@ -35,34 +35,45 @@ def generate_angka(jenis):
     hasil = f'{huruf_pertama}{angka_baru}'  # Format ID baru
     return hasil
 
-# 2. Membaca mobil_list (READ)
+#FUNGSI PILIHAN LANJUT YA/TIDAK
+def lanjutkan(callback):
+    while True:
+        masih_pesanan = validasi_alfabet('Ada lagi? (Ya/Tidak): ').capitalize()
+        if masih_pesanan == 'Ya':
+            callback()
+            break
+        elif masih_pesanan == 'Tidak':
+            menu_utama()
+            break
+        else:
+            print("\nMasukan Ya dan Tidak Saja!\n")
+# 2. Membaca Tabulasi (READ)
 def read_mobil():
-    print("=== Daftar Rental Mobil Kami ===")
+    print("\n=== Daftar Rental Mobil Kami ===")
     print(tabulate(mobil_list, headers='keys', tablefmt='fancy_grid')) #menunjukan tabel stok
     print(' ')
 def transaksi_mobil():
-    print("           === Booked! ===")
+    print("\n           === Booked! ===")
     print(tabulate(transaksi, headers='keys', tablefmt='fancy_grid')) #menunjukan tabel transaksi
 def tong_sampah():
     print("           === Recycle Bin! ===")
     print(tabulate(recycle_bin, headers='keys', tablefmt='fancy_grid')) #menunjukan tabel transaksi
 
-# 3. Membuat validasi input
+# 3. Membuat Fungsi validasi input
 def validasi_alfabet(prompt): #validasi untuk input huruf
     while True:
-        inputan = input(prompt)
+        inputan = input(prompt).strip()
         if inputan.isalpha():
             return inputan
         else:
-            print("Inputan harus berupa huruf!")
-
+            print("\nInputan harus berupa huruf!")
 def validasi_angka(prompt): #validasi untuk input angka
     while True:
-        inputan = input(prompt)
+        inputan = input(prompt).strip()
         if inputan.isdigit():
             return int(inputan)
         else:
-            print("Inputan harus berupa angka!")
+            print("\nInputan harus berupa angka!")
 
 # 4. Isi info kendaraan baru (CREATE)
 def info():
@@ -77,20 +88,25 @@ def info():
         
         mobil_list.append(mobil_baru) #menambah inputan kedalam list dict
         return read_mobil() #kembali menunjukan list tabel terbaru
-print(' ')
 
 # 5. Mengupdate stok yang ada (UPDATE)
 def update_stok():
-    update_tabel = input('Masukan ID. Mobil yang ingin diupdate (Ketik 0 Jika Batal): ').capitalize()
-    if update_tabel == 0:
-        return 
+    update_tabel = input('\nMasukan ID. Mobil yang ingin diupdate (Ketik 0 Jika Batal): ').capitalize().strip()
+    if update_tabel == '0':
+        return
+    elif update_tabel not in [mobil['ID'] for mobil in mobil_list]:
+        print ('\nData tidak ditemukan! Masukan data yang Valid')
+        return update_stok()
+    
+    
     #Checking data yang diupdate
     for item in mobil_list:
         if item ['ID'] == update_tabel:
             while True:
-                update = validasi_alfabet('Masukan data yang ingin diperbarui (Jenis/Harga/Kapasitas/Stok): ').capitalize()
+                update = validasi_alfabet('\nMasukan data yang ingin diperbarui (Jenis/Harga/Transmisi/Kapasitas/Stok): ').capitalize()
                 if update == 'Jenis':
                     item['Jenis'] = validasi_alfabet('Masukan Jenis Mobil Baru: ').capitalize()
+                    item['ID'] = generate_angka(item['Jenis'])
                 elif update == 'Transmisi':
                     item['Transmisi'] = transmisi()
                 elif update == 'Harga':
@@ -100,40 +116,38 @@ def update_stok():
                 elif update == 'Stok':
                     item['Stok'] = validasi_angka('Masukan Stok Baru: ')
                 else:
-                    print('Data tidak ditemukan. Masukan data yang Valid')
+                    print('\nData tidak ditemukan. Masukan data yang Valid')
+                    continue
                 
-                masih_update = validasi_alfabet('Ada lagi? (Ya/Tidak): ').capitalize()
-                if masih_update != 'Ya':
-                    read_mobil()
-                    break
-                print('Data berhasil Diperbarui')
-print(' ')
+                lanjutkan(update_stok)
+                break
 
 # 6. Menghapus data yang sudah ada (DELETE)
 def hapus_data():
-    hapus_tabel = input('Masukan ID. Mobil yang ingin dihapus (Ketik 0 Jika Batal): ').capitalize()
-    if hapus_tabel == 0:
-        return 
+    hapus_tabel = input('\nMasukan ID. Mobil yang ingin dihapus (Ketik 0 Jika Batal): ').capitalize()
+    if hapus_tabel == '0':
+        return
+    elif hapus_tabel not in [mobil['ID'] for mobil in mobil_list]:
+        print ('\nData tidak ditemukan! Masukan data yang Valid')
+        return hapus_data()
         
     for item in mobil_list:
         if item['ID'] == hapus_tabel:
             mobil_list.remove(item)
             recycle_bin.append(item)
-            print(' ')
-            print('                          Data berhasil dihapus')
+            print('\n                          Data berhasil dihapus')
             read_mobil()
             break
            
     else:
-        return print("Mobil tidak ditemukan! Kembali ke menu.")
-print(' ')
-
+        return print("\nMobil tidak ditemukan! Kembali ke menu.")
 
 # 7. Menambah data Transaksi (ADDED FEATURE)
-# Fungsi untuk melakukan filter dan sortir mobil
+# Fungsi Sortir Mobil
 def filter_mobil():
     while True:
         print("\n=== Filter Mobil ===")
+        print(' ')
         print("1. Urutkan berdasarkan Harga")
         print("2. Filter berdasarkan Jenis Transmisi")
         print("3. Urutkan berdasarkan Kapasitas")
@@ -144,13 +158,13 @@ def filter_mobil():
         
         # Urutkan berdasarkan harga
         if pilihan == 1:
-            sort_order = validasi_alfabet("Urutkan harga dari rendah ke tinggi atau tinggi ke rendah? (rendah/tinggi): ").lower()
+            sort_order = validasi_alfabet("\nUrutkan harga dari rendah ke tinggi atau tinggi ke rendah? (rendah/tinggi): ").lower()
             if sort_order == "rendah":
                 sorted_mobil = sorted(mobil_list, key=lambda x: x["Harga"])
             elif sort_order == "tinggi":
                 sorted_mobil = sorted(mobil_list, key=lambda x: x["Harga"], reverse=True)
             else:
-                print("Input tidak valid!")
+                print("\nInput tidak valid!")
                 continue
             
             print("\n=== Hasil Filter Harga ===")
@@ -158,9 +172,9 @@ def filter_mobil():
 
         # Filter berdasarkan transmisi
         elif pilihan == 2:
-            transmisi_filter = validasi_alfabet("Masukkan jenis transmisi yang dicari (AT/MT): ").upper().strip()
+            transmisi_filter = validasi_alfabet("\nMasukkan jenis transmisi yang dicari (AT/MT): ").upper().strip()
             if transmisi_filter not in ["AT", "MT"]:
-                print("Input tidak valid! Pilih AT atau MT.")
+                print("\nInput tidak valid! Pilih AT atau MT.")
                 continue
             
             filtered_mobil = [mobil for mobil in mobil_list if mobil["Transmisi"].strip() == transmisi_filter]
@@ -170,13 +184,13 @@ def filter_mobil():
 
         # Urutkan berdasarkan kapasitas
         elif pilihan == 3:
-            sort_order = validasi_alfabet("Urutkan kapasitas dari kecil ke besar atau besar ke kecil? (kecil/besar): ").lower()
+            sort_order = validasi_alfabet("\nUrutkan kapasitas dari kecil ke besar atau besar ke kecil? (kecil/besar): ").lower()
             if sort_order == "kecil":
                 sorted_mobil = sorted(mobil_list, key=lambda x: x["Kapasitas"])
             elif sort_order == "besar":
                 sorted_mobil = sorted(mobil_list, key=lambda x: x["Kapasitas"], reverse=True)
             else:
-                print("Input tidak valid!")
+                print("\nInput tidak valid!")
                 continue
 
             print("\n=== Hasil Filter Kapasitas ===")
@@ -186,16 +200,19 @@ def filter_mobil():
         elif pilihan == 4:
             return
         else:
-            print("Pilihan tidak valid! Silakan coba lagi.")
+            print("\nPilihan tidak valid! Silakan coba lagi.")
 print(' ')
 
 
 # FUNGSI BOOKING/TAMBAH TRANSAKSI
 def tambah_transaksi():
     read_mobil()
-    pesanan = input('Masukan ID. Mobil yang ingin disewa (Ketik 0 untuk batal): ').capitalize()
+    pesanan = input('\nMasukan ID. Mobil yang ingin disewa (Ketik 0 untuk batal): ').capitalize()
     if pesanan == "0":
         return
+    elif pesanan not in [mobil['ID'] for mobil in mobil_list]:
+        print ('\nData tidak ditemukan! Masukan data yang Valid\n')
+        return tambah_transaksi()
     
     for mobil in mobil_list:
         if pesanan in [mobil['ID'] for mobil in mobil_list]:
@@ -209,24 +226,23 @@ def tambah_transaksi():
                                            "Harga": mobil['Harga'],
                                              "Jumlah": jumlah,
                                                "Total": mobil['Harga']*jumlah})
-                    print(' ')
-                    print(f"=== Berhasil menyewa {jumlah} unit {mobil['Jenis']}! ===")
-                    return transaksi_mobil()
+                    print(f"\n=== Berhasil menyewa {jumlah} unit {mobil['Jenis']}! ===")
+                    transaksi_mobil()
                 else:
-                    print("Stok tidak mencukupi!\n")
-                    return
-        else:
-            print("Mobil tidak ditemukan!\n")
-print(' ')
+                    print("\nStok tidak mencukupi!\n")
+                    return tambah_transaksi()
+                
+                lanjutkan(tambah_transaksi)
+                break  
         
             
 # 8. Menu utama untuk memilih (MENU)
 def menu_utama():
     while True:
-        print('--- 𝐖 𝐄 𝐋 𝐂 𝐎 𝐌 𝐄 ---')
-        print("\n=== Menu Administrasi ===")
+        print('\n--- 𝐖 𝐄 𝐋 𝐂 𝐎 𝐌 𝐄 ---')
+        print("\n=== Menu Administrasi ===\n")
         print("1. Lihat Daftar Mobil")
-        print("2. Isi Info Kendaraan Baru")
+        print("2. Isi Daftar Kendaraan Baru")
         print("3. Update Tabel")
         print("4. Hapus Data")
         print("5. Tampilkan Recyle Bin")
@@ -252,8 +268,6 @@ def menu_utama():
         elif pilihan == 7:
             filter_mobil()
         else:
-            print("Terima kasih!")
-            break
-print()
-
+            print("\n================= Terima kasih! =================\n")
+            break 
 menu_utama()
